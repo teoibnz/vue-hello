75c663bdce25d87c484e2c7a0857601ddd542bb5
Matteo Lopez <matteolopezbb@gmail.com>
2022-03-16 16:42:32 +0100

    inserimento di un'immagine inserendo l'url nel file js

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const app = new Vue (
         el : '#app',
         data : {
             message: 'Ciao',
+            image : 'https://static.toiimg.com/thumb/msid-80009581,width-1070,height-580,imgsize-1353811,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg',
+            
         }
         
     }
